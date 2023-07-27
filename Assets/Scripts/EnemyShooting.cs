b92b66922a8eba10fb5366bd633db9694e7c2193
SLIVAL <samuel.coa48@gmail.com>
2023-07-27 17:37:26 +1000

    Player Detection
    Added a few methods for enemies to detect when the player is within a certain range and is in line of sight.

--- Assets/Scripts/EnemyShooting.cs
+++ Assets/Scripts/EnemyShooting.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
 {
+    [Header("Player Detection")]
     private GameObject player;
+    [SerializeField] private float detectionDistance;
+    private float distanceFromPlayer;
 
-    void Start()
+    [Header("Projectile Properties")]
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private Vector3 projectileSize;
+    [SerializeField] private float projectileLifespan;
+
+    private void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ProcessDetection();
+    }
+
+    // Handles player detection and calling necessary methods
+    private void ProcessDetection()
+    {
+        distanceFromPlayer = Vector3.Distance(player.transform.position, this.transform.position);
+
+        if(distanceFromPlayer <= detectionDistance && IsPlayerVisible())
+        {
+            // shoot
+            Debug.Log("Player detected");
+        }
+    }
+
+    // Checks if player is within line of sight
+    private bool IsPlayerVisible()
+    {
+        Vector3 raycastDirection = player.transform.position - this.transform.position;
+        RaycastHit hit;
+        if(Physics.Raycast(this.transform.position, raycastDirection, out hit))
+        {
+            if(hit.transform.CompareTag("Player"))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
