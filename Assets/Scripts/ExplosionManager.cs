c6c044f7ab72e2b0ae8b3c10c593fea0af2ec7df
Quang Duong <quanglienduong@gmail.com>
2023-08-02 19:33:51 +1000

    Added Explosion Sphere to Display Blast Radius

--- Assets/Scripts/ExplosionManager.cs
+++ Assets/Scripts/ExplosionManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ExplosionManager : MonoBehaviour
 {
-    public GameObject explosion;
+    public GameObject explosionParticles; // The gameobject to play the particle systems in the children during detonation
+    public GameObject explosionSphere; // The gameobject to show the radius of the explosion
 
     static ExplosionManager manager;
 
@@ -20,11 +21,14 @@ public class ExplosionManager : MonoBehaviour
 
     }*/
 
+    // Create a explosion at "position" of size "radius", pushing out all rigidbodies at a force plus additional upwardForce.
+    // "lifetime" affects how long Explosion FX stay active.
     public static void CastExplosion(Vector3 position, float radius, float force = 200.0f, float upwardForce = 50.0f, float lifetime = 1.2f)
     {
-        ExplodeFX(position, lifetime);
+        ExplodeFX(position, radius, lifetime);
         Collider[] entities = Physics.OverlapSphere(position, radius);
 
+        // Look for all rigidbodies and apply explosion force onto them
         for (int i = 0; i < entities.Length; i++)
         {
             if (entities[i].attachedRigidbody)
@@ -37,19 +41,22 @@ public class ExplosionManager : MonoBehaviour
 
     public void TestExplosion()
     {
-        CastExplosion(Vector3.zero, 20, 300);
+        CastExplosion(Vector3.zero, 10, 300);
     }
 
-    static void ExplodeFX(Vector3 position, float lifetime)
+    // Method to call ExplosionFX statically
+    static void ExplodeFX(Vector3 position, float radius, float lifetime)
     {
         manager.StartCoroutine(manager.ExplosionFX(position, lifetime));
+        manager.StartCoroutine(manager.ExplosionSphere(position, radius, 0.2f, lifetime));
     }
 
+    // Instantiate "explosionParitcles" gameobject and play all particle systems in children and destroy after "lifetime" seconds.
     IEnumerator ExplosionFX(Vector3 position, float lifetime)
     {
-        if (explosion)
+        if (explosionParticles)
         {
-            GameObject temp = Instantiate(explosion, position, new Quaternion(), gameObject.transform);
+            GameObject temp = Instantiate(explosionParticles, position, new Quaternion(), gameObject.transform);
             ParticleSystem[] particles = temp.GetComponentsInChildren<ParticleSystem>();
 
             for (int i = 0; i < particles.Length; i++)
@@ -60,5 +67,39 @@ public class ExplosionManager : MonoBehaviour
             yield return new WaitForSeconds(lifetime);
             Destroy(temp);
         }
+    }
+
+    // Instantiate a sphere to show the explosion radius. The sphere grows to full size within "growthTime" seconds and fades away within "lifetime" seconds.
+    IEnumerator ExplosionSphere(Vector3 position, float radius, float growthTime, float lifetime)
+    {
+        float time = 0;
+
+        GameObject temp = Instantiate(explosionSphere, position, new Quaternion(), gameObject.transform);
+        Transform tempt = temp.transform;
+        Material material = temp.GetComponent<Renderer>().material;
+        Color color = material.GetColor("_Color");
+
+        float alpha = color.a;
+        tempt.localScale = Vector3.zero;
+
+        while (time < lifetime)
+        {
+            if (time <= growthTime)
+            {
+                float growValue = Mathf.Lerp(0, radius * 2, 1 - Mathf.Pow(1 - (time / growthTime), 5));
+                tempt.localScale = new Vector3(growValue, growValue, growValue);
+            }
+            else
+            {
+                float a = Mathf.Lerp(color.a, 0, (time - growthTime) / (lifetime - growthTime));
+                material.SetColor("_Color", new Color(color.r, color.g, color.b, a));
+            }
+
+            time += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        Destroy(temp);
+        yield return null;
     }
 }
