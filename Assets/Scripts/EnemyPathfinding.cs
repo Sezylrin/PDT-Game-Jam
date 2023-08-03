c8ebe7b95fa75d4dc40b18e702cddd279f68c8ff
SLIVAL <samuel.coa48@gmail.com>
2023-08-03 16:24:41 +1000

    Improved Patrol Behaviour
    The enemy will patrol a circular area (like a post) when not following the player.

--- Assets/Scripts/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -6,21 +6,27 @@ public class EnemyPathfinding : MonoBehaviour
 {
     private EnemyShooting EnemyShooting;
     private Pathfinding.AIDestinationSetter AIDestinationSetter;
+    private Pathfinding.AIPath AIPath;
     private GameObject player;
     [SerializeField] private GameObject patrolPoint;
     [SerializeField] float patrolAreaRadius;
+    private GameObject patrolTarget;
 
     private void Awake()
     {
         player = GameObject.FindWithTag(Tags.T_Player);
         EnemyShooting = this.GetComponentInChildren<EnemyShooting>();
         AIDestinationSetter = this.GetComponent<Pathfinding.AIDestinationSetter>();
+        AIPath = this.GetComponent<Pathfinding.AIPath>();
     }
 
     private void Start()
     {
         patrolPoint = new GameObject("AIPatrolPoint");
         patrolPoint.transform.position = this.transform.position;
+
+        patrolTarget = new GameObject("PatrolTarget");
+        patrolTarget.transform.position = patrolPoint.transform.position;
     }
 
     private void Update()
@@ -37,10 +43,21 @@ public class EnemyPathfinding : MonoBehaviour
             return;
         }
 
+        if(AIPath == null)
+        {
+            Debug.Log("AIPath not found. Ensure the script is attached to the enemy AI");
+            return;
+        }
+
         switch(EnemyShooting.enemyState)
         {
+            // Set destination to a random point close to patrolPoint
             case EnemyShooting.EnemyState.Patrolling:
-                AIDestinationSetter.target = patrolPoint.transform;
+                if(AIPath.reachedDestination)
+                {
+                    MovePatrolTarget();
+                }
+                AIDestinationSetter.target = patrolTarget.transform;
                 break;
 
             // Set destination to the player when the player is in range and line of sight
@@ -51,5 +68,13 @@ public class EnemyPathfinding : MonoBehaviour
             default:
                 break;
         }
+    }
+
+    // Randomises the position of patrolTarget within a circular area with patrolPoint as the origin
+    // The size of the circular area is dependant on the patrolAreaRadius float
+    private void MovePatrolTarget()
+    {
+        Vector2 rand = Random.insideUnitCircle * patrolAreaRadius;
+        patrolTarget.transform.position = new Vector3(patrolPoint.transform.position.x + rand.x, patrolPoint.transform.position.y, patrolPoint.transform.position.z + rand.y);
     }
 }
